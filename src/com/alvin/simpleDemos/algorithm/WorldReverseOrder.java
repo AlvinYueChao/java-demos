2311388da5f340dd58aff284204710202bbe7ad3
YueChao <yue327305210@163.com>
2020-02-16 17:02:47 +0800

    added two algorighm tests

---
+++ src/com/alvin/simpleDemos/algorithm/WorldReverseOrder.java
@@ -0,0 +1,27 @@
+package com.alvin.simpleDemos.algorithm;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class WorldReverseOrder {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String testStr = reader.readLine();
+        String[] words = testStr.split(" ");
+        Stack<String> wordsStack = new Stack<>();
+        for (String word : words) {
+            wordsStack.push(word);
+        }
+
+        String[] newWords = new String[words.length];
+        for (int i = 0; i < words.length; i++) {
+            newWords[i] = wordsStack.pop();
+        }
+
+        for (String newWord : newWords) {
+            System.out.print(newWord + " ");
+        }
+    }
+}
