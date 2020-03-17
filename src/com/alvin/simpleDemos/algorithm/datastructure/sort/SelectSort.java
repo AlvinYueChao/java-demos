496f925522fc6e9745bb44b3029c61e3ce9c5bfc
YueChao <yue327305210@163.com>
2020-03-17 21:39:54 +0800

    updated select sort

--- src/com/alvin/simpleDemos/algorithm/datastructure/sort/SelectSort.java
+++ src/com/alvin/simpleDemos/algorithm/datastructure/sort/SelectSort.java
@@ -10,13 +10,16 @@ public class SelectSort {
     }
 
     private static void selectSort(int[] list) {
-        for (int i = 0; i < list.length; i++) {
+        for (int i = 0; i < list.length - 1; i++) {
+            int minIndex = i;
             for (int j = i + 1; j < list.length; j++) {
-                if (list[j] < list[i]) {
-                    int temp = list[i];
-                    list[i] = list[j];
-                    list[j] = temp;
-                }
+                minIndex = list[i] < list[j] ? i : j;
+            }
+
+            if (minIndex != i) {
+                int temp = list[i];
+                list[i] = list[minIndex];
+                list[minIndex] = temp;
             }
         }
     }
