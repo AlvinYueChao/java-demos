7f242bd044dbb13618bc806fb2c969e829c0b243
YueChao <yue327305210@163.com>
2020-03-28 00:05:20 +0800

    tree traversal

---
+++ src/com/alvin/simpleDemos/algorithm/datastructure/tree/TreeBin.java
@@ -0,0 +1,13 @@
+package com.alvin.simpleDemos.algorithm.datastructure.tree;
+
+public class TreeBin<T> {
+    T value;
+    TreeBin<T> left;
+    TreeBin<T> right;
+
+    TreeBin(T value) {
+        this.value = value;
+        left = null;
+        right = null;
+    }
+}
