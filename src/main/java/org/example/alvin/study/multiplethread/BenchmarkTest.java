7781f7daadfe6ba508fedf60a9f2185deb2947f1
YueChao <yue327305210@163.com>
2021-03-14 22:00:12 +0800

    20210314 2200

---
+++ src/main/java/org/example/alvin/study/multiplethread/BenchmarkTest.java
@@ -0,0 +1,40 @@
+package org.example.alvin.study.multiplethread;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.Options;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author AlvinPower
+ * @date 2021/3/14
+ */
+@Fork(1)
+@BenchmarkMode(Mode.AverageTime)
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+public class BenchmarkTest {
+    public static void main(String[] args) throws RunnerException {
+        Options opt = new OptionsBuilder().include(BenchmarkTest.class.getSimpleName()).build();
+        new Runner(opt).run();
+    }
+
+    @Benchmark
+    public void a() {
+        int i = 0;
+        i++;
+    }
+
+    @Benchmark
+    public void b() {
+        int i = 0;
+        Object o = new Object();
+        synchronized (o) {
+            i++;
+        }
+    }
+}
