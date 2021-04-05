adbdd32b288afb37f93a4576951e6e781671b61a
YueChao <yue327305210@163.com>
2021-04-05 16:42:29 +0800

    20210405 1642

--- src/main/java/org/example/alvin/study/multiplethread/ThreadPoolTest.java
+++ src/main/java/org/example/alvin/study/multiplethread/ThreadPoolTest.java
@@ -2,8 +2,7 @@ package org.example.alvin.study.multiplethread;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 /**
  * @author AlvinPower
@@ -12,22 +11,49 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class ThreadPoolTest {
     public static void main(String[] args) {
-        ExecutorService pool1 = Executors.newFixedThreadPool(1);
+        ExecutorService pool1 = new ThreadPoolExecutor(2, 2, 0, TimeUnit.MILLISECONDS, new ArrayBlockingQueue<>(1));
+
+        log.info("main start");
 
         pool1.execute(() -> {
-            log.info("1");
+            log.info("1 start");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+            log.info("1 end");
         });
 
         pool1.execute(() -> {
-            log.info("2");
-            int k = 1/0;
+            log.info("2 start");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+            log.info("2 end");
         });
 
         pool1.execute(() -> {
-            log.info("3");
+            log.info("3 start");
+            try {
+                TimeUnit.SECONDS.sleep(1);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                Thread.currentThread().interrupt();
+            }
+            log.info("3 end");
         });
+
+        log.info("main end");
+        pool1.shutdown();
+        pool1.shutdownNow();
 
         /*
+        ExecutorService pool1 = Executors.newFixedThreadPool(1);
         ExecutorService pool2 = Executors.newCachedThreadPool();
         ExecutorService pool3 = Executors.newSingleThreadExecutor();
         ScheduledExecutorService pool4 = Executors.newScheduledThreadPool(1);
