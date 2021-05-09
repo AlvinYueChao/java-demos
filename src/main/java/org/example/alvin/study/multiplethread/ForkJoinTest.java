7cea1ef7d47220ece5a6bbec7ddafa15af4255da
YueChao <yue327305210@163.com>
2021-05-09 22:01:47 +0800

    20210509 2201

---
+++ src/main/java/org/example/alvin/study/multiplethread/ForkJoinTest.java
@@ -0,0 +1,32 @@
+package org.example.alvin.study.multiplethread;
+
+import java.util.concurrent.ForkJoinPool;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author AlvinPower
+ * @date 2021/4/18
+ */
+@Slf4j
+public class ForkJoinTest {
+
+  public static void main(String[] args) {
+    ForkJoinPool pool = new ForkJoinPool(4);
+    pool.submit(() -> {
+      log.info("{} executing task", Thread.currentThread().getName());
+    });
+    pool.submit(() -> {
+      log.info("{} executing task", Thread.currentThread().getName());
+    });
+    pool.submit(() -> {
+      log.info("{} executing task", Thread.currentThread().getName());
+    });
+    pool.submit(() -> {
+      log.info("{} executing task", Thread.currentThread().getName());
+    });
+    pool.submit(() -> {
+      log.info("{} executing task", Thread.currentThread().getName());
+    });
+    log.info("Main completed");
+  }
+}
