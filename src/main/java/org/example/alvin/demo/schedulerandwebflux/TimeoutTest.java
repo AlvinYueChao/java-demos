101e650b489e858db8eb298a3ca00067e7819945
YueChao <yue327305210@163.com>
2021-06-22 22:01:48 +0800

    mono test

---
+++ src/main/java/org/example/alvin/demo/schedulerandwebflux/TimeoutTest.java
@@ -0,0 +1,54 @@
+package org.example.alvin.demo.schedulerandwebflux;
+
+import java.time.Duration;
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Scheduler;
+import reactor.core.scheduler.Schedulers;
+
+/**
+ * @author AlvinPower
+ * @date 2021/6/22
+ */
+@Slf4j
+public class TimeoutTest {
+
+  public static void main(String[] args) {
+    boSubscribeDemo();
+  }
+
+  private static void boSubscribeDemo() {
+    Mono<String> resultMono1 = mockRestfulRequest1();
+    Mono<String> resultMono2 = mockRestfulRequest2();
+
+  }
+
+  private static void subscribeLast_withElasticSchedulerDemo() {
+    Scheduler elastic = Schedulers.elastic();
+    Mono<String> resultMono1 = mockRestfulRequest1();
+    Mono<String> resultMono2 = mockRestfulRequest2();
+
+  }
+
+  private static Mono<String> mockRestfulRequest1() {
+    log.info("Working on processing request...");
+    try {
+      Thread.sleep(2000);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      e.printStackTrace();
+    }
+    return Mono.just("successful result #1");
+  }
+
+  private static Mono<String> mockRestfulRequest2() {
+    log.info("Working on processing request...");
+    try {
+      Thread.sleep(3000);
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+      e.printStackTrace();
+    }
+    return Mono.just("successful result #2");
+  }
+}
