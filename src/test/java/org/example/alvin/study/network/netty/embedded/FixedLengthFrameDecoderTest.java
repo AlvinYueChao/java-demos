bd9cca36ad91f46dc45f38e645d14bbedcbbc2e0
YueChao <yue327305210@163.com>
2021-07-06 20:33:17 +0800

    updated fixedLengthFrameDecoder test case

--- src/test/java/org/example/alvin/study/network/netty/embedded/FixedLengthFrameDecoderTest.java
+++ src/test/java/org/example/alvin/study/network/netty/embedded/FixedLengthFrameDecoderTest.java
@@ -21,5 +21,16 @@ class FixedLengthFrameDecoderTest {
     ByteBuf input = buf.duplicate();
     EmbeddedChannel channel = new EmbeddedChannel(new FixedLengthFrameDecoder(3));
     Assertions.assertFalse(channel.writeInbound(input.readBytes(1)));
+    Assertions.assertNull(channel.readInbound());
+    Assertions.assertFalse(channel.writeInbound(input.readBytes(1)));
+    Assertions.assertNull(channel.readInbound());
+    // writeInbound 返回值代表 readInbound() 能否从该 channel 中读取到结果. 只有写入长度达到固定长度，才会触发 channel 的 flush 操作
+    Assertions.assertTrue(channel.writeInbound(input.readBytes(1)));
+    Assertions.assertNotNull(channel.readInbound());
+
+    // buf 中最多只能再读取6个字节的数据，如果超出写入字节总数，会抛异常 java.lang.IndexOutOfBoundsException
+    Assertions.assertTrue(channel.writeInbound(input.readBytes(6)));
+    Assertions.assertNotNull(channel.readInbound());
+    channel.finish();
   }
 }
