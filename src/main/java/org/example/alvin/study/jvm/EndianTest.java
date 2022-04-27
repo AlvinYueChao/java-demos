b33924b9507a5bc5a9615902ecf821cc90cfb632
YueChao <yue327305210@163.com>
2022-04-27 22:18:22 +0800

    20220427 2218

---
+++ src/main/java/org/example/alvin/study/jvm/EndianTest.java
@@ -0,0 +1,32 @@
+package org.example.alvin.study.jvm;
+
+import java.nio.ByteOrder;
+import lombok.extern.slf4j.Slf4j;
+import sun.misc.Unsafe;
+
+@Slf4j
+public class EndianTest {
+
+  public static void main(String[] args) {
+    Unsafe unsafe = UnsafeFactory.getUnsafeInstance();
+    long a = unsafe.allocateMemory(8);
+    try {
+      unsafe.putLong(a, 0x0102030405060708L);
+      byte b = unsafe.getByte(a);
+      ByteOrder byteOrder;
+      switch (b) {
+        case 0x01:
+          byteOrder = ByteOrder.BIG_ENDIAN;
+          break;
+        case 0x08:
+          byteOrder = ByteOrder.LITTLE_ENDIAN;
+          break;
+        default:
+          byteOrder = null;
+      }
+      log.info("{}", byteOrder);
+    } finally {
+      unsafe.freeMemory(a);
+    }
+  }
+}
