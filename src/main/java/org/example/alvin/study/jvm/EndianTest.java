32d1c2c16fa0e2b0e2fd1b65aca381fee77dc575
YueChao <yue327305210@163.com>
2022-05-01 11:20:43 +0800

    20220501 1120

--- src/main/java/org/example/alvin/study/jvm/EndianTest.java
+++ src/main/java/org/example/alvin/study/jvm/EndianTest.java
@@ -2,13 +2,12 @@ package org.example.alvin.study.jvm;
 
 import java.nio.ByteOrder;
 import lombok.extern.slf4j.Slf4j;
-import sun.misc.Unsafe;
 
 @Slf4j
 public class EndianTest {
 
   public static void main(String[] args) {
-    Unsafe unsafe = UnsafeFactory.getUnsafeInstance();
+    var unsafe = UnsafeFactory.getUnsafeInstance();
     long a = unsafe.allocateMemory(8);
     try {
       unsafe.putLong(a, 0x0102030405060708L);
