ffafeeb57c127243564a6c3f32e8501b8b7d3870
YueChao <yue327305210@163.com>
2022-05-29 15:25:17 +0800

    20220529 1525

--- src/main/java/org/example/alvin/study/network/netty/http/server/BusinessHandler.java
+++ src/main/java/org/example/alvin/study/network/netty/http/server/BusinessHandler.java
@@ -1,7 +1,11 @@
 package org.example.alvin.study.network.netty.http.server;
 
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
 /**
  * @author AlvinPower
  * @date 2021/6/17
  */
-public class BusinessHandler {}
+public class BusinessHandler extends ChannelInboundHandlerAdapter {
+
+}
--- src/main/java/org/example/alvin/study/network/netty/http/server/ServerHandlerInit.java
+++ src/main/java/org/example/alvin/study/network/netty/http/server/ServerHandlerInit.java
@@ -1,7 +1,37 @@
 package org.example.alvin.study.network.netty.http.server;
 
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpContentCompressor;
+import io.netty.handler.codec.http.HttpObjectAggregator;
+import io.netty.handler.codec.http.HttpRequestDecoder;
+import io.netty.handler.codec.http.HttpResponseEncoder;
+import io.netty.handler.ssl.SslContext;
+
 /**
  * @author AlvinPower
  * @date 2021/6/17
  */
-public class ServerHandlerInit {}
+public class ServerHandlerInit extends ChannelInitializer<SocketChannel> {
+
+  private SslContext sslContext;
+  private static final int MAX_CONTENT_LENGTH = 10 * 1024 * 1024;
+
+  public ServerHandlerInit(SslContext sslContext) {
+    this.sslContext = sslContext;
+  }
+
+  @Override
+  protected void initChannel(SocketChannel socketChannel) throws Exception {
+    ChannelPipeline ph = socketChannel.pipeline();
+    // 服务器是解码httpRequest，编码httpResponse;客户端反过来
+    ph.addLast("decoder", new HttpRequestDecoder());
+    ph.addLast("encoder", new HttpResponseEncoder());
+    // 聚合http报文，防止自动切片
+    ph.addLast("aggregator", new HttpObjectAggregator(MAX_CONTENT_LENGTH));
+    // 压缩http报文，可选
+    ph.addLast("compressor", new HttpContentCompressor());
+    ph.addLast(new BusinessHandler());
+  }
+}
