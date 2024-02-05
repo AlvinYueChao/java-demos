392c476ccac31c8c62015cb9fd49c5a80d3f93a1
YueChao <yue327305210@163.com>
2024-02-05 21:19:05 +0800

    updates project structure

--- src/main/java/org/example/alvin/study/rocketmq/example/LocalConstant.java
+++
@@ -1,9 +0,0 @@
-package org.example.alvin.study.rocketmq.example;
-
-/**
- * @author AlvinPower
- * @date 2021/5/23
- */
-public class LocalConstant {
-  public static final String NAME_SERVER_ADDR = "192.168.20.13:9876";
-}
