cf64ab4c3d36aa24c91d16aacf97d067012d9ab3
Yue Chao <yue327305210@163.com>
2025-10-10 22:11:01 +0800

    ✨feat(leetcode): bit calc test

--- leetcode-algorithm/src/main/java/org/example/alvin/algorithm/Solution.java
+++ leetcode-algorithm/src/main/java/org/example/alvin/algorithm/Solution.java
@@ -1,10 +1,21 @@
 package org.example.alvin.algorithm;
 
+import java.util.Map;
+import java.util.Map.Entry;
+
 public class Solution {
   public static void main(String[] args) {
-      int a = 10 >>> 1;
-      System.out.println(a);
+
   }
 
+  private static void charCalcTest() {
+    int a1 = '2' - '0' - 1;
+    int a2 = (int) '2' - '1';
+    System.out.println(a1 == a2);
+  }
 
+  private static void logicalBitMovingTest() {
+    int a = 10 >>> 1;
+    System.out.println(a);
+  }
 }
