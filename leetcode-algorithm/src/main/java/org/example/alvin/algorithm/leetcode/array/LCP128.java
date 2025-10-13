01da56e7bd19488c99dccbaf9e952bbb2ff0147c
Yue Chao <yue327305210@163.com>
2025-10-13 14:19:52 +0800

    ✨feat(leetcode): 49. 字母异位词分组 128. 最长连续序列

---
+++ leetcode-algorithm/src/main/java/org/example/alvin/algorithm/leetcode/array/LCP128.java
@@ -0,0 +1,36 @@
+package org.example.alvin.algorithm.leetcode.array;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class LCP128 {
+  public static void main(String[] args) {
+
+  }
+
+  public int longestConsecutive(int[] nums) {
+    Set<Integer> set = new HashSet<>();
+    if(nums.length < 2) {
+      return nums.length;
+    } else {
+      for(int i = 0; i < nums.length; i++) {
+        set.add(nums[i]);
+      }
+
+      int ans = 0;
+      for(int i = 0; i < nums.length; i++) {
+        if(set.contains(nums[i] - 1)) {
+          continue;
+        }
+        int next = nums[i] + 1;
+        int tmpMaxLength = 1;
+        while(set.contains(next)) {
+          next++;
+          tmpMaxLength++;
+        }
+        ans = Math.max(ans, tmpMaxLength);
+      }
+      return ans;
+    }
+  }
+}
---
+++ leetcode-algorithm/src/main/java/org/example/alvin/algorithm/leetcode/array/LCP49.java
@@ -0,0 +1,34 @@
+package org.example.alvin.algorithm.leetcode.array;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class LCP49 {
+  public static void main(String[] args) {
+    String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+    List<List<String>> ans = new LCP49().groupAnagrams(strs);
+    System.out.println(ans);
+  }
+
+  public List<List<String>> groupAnagrams(String[] strs) {
+    List<List<String>> ans = new ArrayList<>();
+
+    Map<String, List<String>> map = new HashMap<>();
+    for (String str : strs) {
+      char[] arr = new char[26];
+      for (char c : str.toCharArray()) {
+        int index = c - 'a';
+        arr[index]++;
+      }
+      String key = new String(arr);
+
+      map.putIfAbsent(key, new ArrayList<>());
+      map.get(key).add(str);
+    }
+
+//    map.forEach((k, v) -> ans.add(v));
+    ans.addAll(map.values());
+
+    return ans;
+  }
+}
