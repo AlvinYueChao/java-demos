ec125066a9a6e096538d20eb0e1bad5b7929b105
Yue Chao <yue327305210@163.com>
2025-11-09 19:42:52 +0800

    ✨feat(leetcode): 207. 课程表

--- algorithm/src/main/java/org/example/alvin/algorithm/leetcode/graph/LCP207.java
+++ algorithm/src/main/java/org/example/alvin/algorithm/leetcode/graph/LCP207.java
@@ -1,9 +1,6 @@
 package org.example.alvin.algorithm.leetcode.graph;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class LCP207 {
   public static void main(String[] args) {
@@ -11,42 +8,40 @@ public class LCP207 {
   }
 
   public boolean canFinish(int numCourses, int[][] prerequisites) {
-    Map<Integer, Integer> map1 = new HashMap<>(); // 课程-课程的前置课程数，当且仅当前置课程数为0时，可学习该课程
-    Map<Integer, List<Integer>> map2 = new HashMap<>(); // 课程-以该课程作为前置课程的所有课程
-    List<Integer> res = new ArrayList<>(); // 已完成课程
+    return bfsImpl(numCourses, prerequisites);
+  }
 
+  private static boolean bfsImpl(int numCourses, int[][] prerequisites) {
+    List<List<Integer>> courseAndItsPostCourses = new ArrayList<>();
+    int[] preCourseCount = new int[numCourses];
     for (int i = 0; i < numCourses; i++) {
-      map1.put(i, 0);
-      map2.put(i, new ArrayList<>());
+      courseAndItsPostCourses.add(new ArrayList<>());
+    }
+    for (int[] prerequisite : prerequisites) {
+      courseAndItsPostCourses.get(prerequisite[1]).add(prerequisite[0]);
+      preCourseCount[prerequisite[0]]++;
     }
 
-    for (int[] coursePair : prerequisites) {
-      // 前置课程数加一
-      Integer preCount = map1.get(coursePair[0]);
-      map1.put(coursePair[0], ++preCount);
-      // 记录后置课程
-      map2.get(coursePair[1]).add(coursePair[0]);
+    Queue<Integer> queue = new LinkedList<>();
+    for (int i = 0; i < numCourses; i++) {
+      if (preCourseCount[i] == 0) {
+        queue.offer(i);
+      }
     }
 
-    int nextCourse = 0;
-    while (!map1.isEmpty()) {
-      for(Map.Entry<Integer, Integer> entry : map1.entrySet()) {
-        if (entry.getValue() == 0) {
-          res.add(entry.getKey());
+    int visited = 0;
+    while (!queue.isEmpty()) {
+      visited++;
+
+      int course = queue.poll();
+      for (int postCourse : courseAndItsPostCourses.get(course)) {
+        preCourseCount[postCourse]--;
+        if (preCourseCount[postCourse] == 0) {
+          queue.offer(postCourse);
         }
       }
-      if (nextCourse == res.size()) {
-        return false;
-      }
-      for(int i = nextCourse; i < res.size(); i++) {
-        map1.remove(res.get(i));
-        for(Integer course : map2.get(res.get(i))) {
-          map1.put(course, map1.get(course) - 1);
-        }
-      }
-      nextCourse = res.size();
     }
 
-    return res.size() == numCourses;
+    return visited == numCourses;
   }
 }
